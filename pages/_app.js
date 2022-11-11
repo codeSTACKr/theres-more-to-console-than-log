946a5e377744add3bd3ffac9a9d7132aa8281efd
codeSTACKr <codestackr@gmail.com>
2022-11-11 13:53:26 -0600

    dubugger

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>There's More to Console than .log()</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
