2c7c903754249aaa7642f174d0a25ad47ef71f82
codeSTACKr <codestackr@gmail.com>
2022-11-11 14:34:32 -0600

    debugger

--- next.config.js
+++ next.config.js
@@ -1,23 +1,20 @@
-const { remarkCodeHike } = require("@code-hike/mdx")
-const theme = require('./theme/codestackr.json');
+const { remarkCodeHike } = require("@code-hike/mdx");
+const theme = require("./theme/codestackr.json");
 
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
-    remarkPlugins: [[remarkCodeHike, { theme,
-      lineNumbers: true }]],
+    remarkPlugins: [[remarkCodeHike, { theme, lineNumbers: true }]],
   },
-})
+});
 
 module.exports = withMDX({
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   eslint: { ignoreDuringBuilds: true },
   reactStrictMode: true,
-  swcMinify: true,
-  webpack: (
-    config,
-    { buildId, dev = true, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    return config
+  swcMinify: false,
+  optimization: {
+    minimize: false,
   },
-})
+  mode: 'development',
+});
